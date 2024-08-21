a7c597698fe103a5a568acf1b20b6780d7c20752
Gustavo Leite da Silva <rm94343@fiap.com.b>
2024-08-20 21:51:05 -0300

    primeiro commit kk.

---
+++ src/main/java/br/com/fiap/code_rats/CodeRatsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.code_rats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeRatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeRatsApplication.class, args);
+	}
+
+}
