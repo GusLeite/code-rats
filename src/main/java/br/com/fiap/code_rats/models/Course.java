805af8b1cb7e9a2cd0b95f00388ae0c198b7b7af
labsfiap <rm49343@fiap.com.br>
2024-08-27 22:19:45 -0300

    Adicionado suporte para banco de dados oracle

--- src/main/java/br/com/fiap/code_rats/models/Course.java
+++ src/main/java/br/com/fiap/code_rats/models/Course.java
@@ -1,19 +1,18 @@
 package br.com.fiap.code_rats.models;
 
 import br.com.fiap.code_rats.dtos.CourseCreationDTO;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @Entity(name = "courses")
 public class Course {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @NotNull
     private String name;
